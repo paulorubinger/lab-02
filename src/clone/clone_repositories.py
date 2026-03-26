52cbe1a1b5f12df3d67ae56f7d7621d8ee42d324
Paulo Rubinger <paulopimentarubinger@gmail.com>
2026-03-26 02:46:57 -0300

    add script para clonar repositórios

---
+++ src/clone/clone_repositories.py
@@ -0,0 +1,44 @@
+import csv
+import os
+import subprocess
+
+INPUT_FILE = "data/raw/top_1000_java_repositories.csv"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+CLONE_FOLDER = os.path.join(BASE_DIR, "repos")
+MAX_REPOS = 1
+
+def create_clone_folder():
+    if not os.path.exists(CLONE_FOLDER):
+        os.makedirs(CLONE_FOLDER)
+
+def clone_repository(repo_url, repo_name):
+    destination = os.path.join(CLONE_FOLDER, repo_name)
+
+    if os.path.exists(destination):
+        print(f"{repo_name} already exists. Skipping...")
+        return
+
+    print(f"Cloning {repo_name}...")
+    subprocess.run(["git", "clone", repo_url, destination])
+
+def clone_top_repositories():
+    create_clone_folder()
+
+    with open(INPUT_FILE, newline="", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+
+        count = 0
+
+        for repo in reader:
+            if count >= MAX_REPOS:
+                break
+
+            repo_url = repo["url"]
+            repo_name = repo["name"].replace("/", "_")
+
+            clone_repository(repo_url, repo_name)
+
+            count += 1
+
+if __name__ == "__main__":
+    clone_top_repositories()
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from github.fetch_repositories import fetch_top_java_repositories
+from clone.clone_repositories import clone_top_repositories
 
 def main():
     fetch_top_java_repositories()
+    clone_top_repositories()
 
 if __name__ == "__main__":
     main()
