2e95019ab1536738897deb59da4e6bf50314efea
Paulo Victor Pimenta Rubinger <paulopimentarubinger@gmail.com>
2026-03-27 13:13:48 -0300

    refactor: refatora clone_repositories.py

--- src/clone/clone_repositories.py
+++ src/clone/clone_repositories.py
@@ -4,21 +4,24 @@ import subprocess
 
 INPUT_FILE = "data/raw/repositories.csv"
 CLONE_FOLDER = "repos"
-MAX_REPOS = 1
+MAX_REPOS = 2
 
 def create_clone_folder():
-    if not os.path.exists(CLONE_FOLDER):
-        os.makedirs(CLONE_FOLDER)
+    os.makedirs(CLONE_FOLDER, exist_ok=True)
 
-def clone_repository(repo_url, repo_name):
+def clone_repository(repo_url, repo_name, index):
     destination = os.path.join(CLONE_FOLDER, repo_name)
 
     if os.path.exists(destination):
         print(f"{repo_name} already exists. Skipping...")
         return
 
-    print(f"Cloning {repo_name}...")
-    subprocess.run(["git", "clone", repo_url, destination])
+    print(f"Cloning repository {index}/{MAX_REPOS}: {repo_name}")
+
+    result = subprocess.run(["git", "clone", repo_url, destination])
+
+    if result.returncode != 0:
+        print(f"Error cloning {repo_name}")
 
 def clone_top_repositories():
     create_clone_folder()
@@ -26,18 +29,14 @@ def clone_top_repositories():
     with open(INPUT_FILE, newline="", encoding="utf-8") as file:
         reader = csv.DictReader(file)
 
-        count = 0
+        for i, repo in enumerate(reader, start=1):
 
-        for repo in reader:
-            if count >= MAX_REPOS:
+            if i > MAX_REPOS:
                 break
 
             repo_url = repo["url"]
-            repo_name = repo["name"].replace("/", "_")
-
-            clone_repository(repo_url, repo_name)
-
-            count += 1
+            repo_name = f"{repo.get('owner')}_{repo.get('repository')}"
+            clone_repository(repo_url, repo_name, i)
 
 if __name__ == "__main__":
     clone_top_repositories()
