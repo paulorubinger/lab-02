b5ffbe5f7f2e5e43f88168ad3cbf3f795a5215e0
Paulo Rubinger <paulopimentarubinger@gmail.com>
2026-04-02 18:13:55 -0300

    feat: add script de clone direto na main

--- src/clone/clone_repositories.py
+++ src/clone/clone_repositories.py
@@ -2,41 +2,38 @@ import csv
 import os
 import subprocess
 
-INPUT_FILE = "data/raw/repositories/repositories.csv"
 CLONE_FOLDER = "repos"
-MAX_REPOS = 5
 
 def create_clone_folder():
     os.makedirs(CLONE_FOLDER, exist_ok=True)
 
-def clone_repository(repo_url, repo_name, index):
+def clone_repository(repo_url, repo_name, index, max_repos):
     destination = os.path.join(CLONE_FOLDER, repo_name)
 
     if os.path.exists(destination):
         print(f"{repo_name} already exists. Skipping...")
         return
 
-    print(f"Cloning repository {index}/{MAX_REPOS}: {repo_name}")
+    print(f"Cloning repository {index}/{max_repos}: {repo_name}")
 
     result = subprocess.run(["git", "clone", repo_url, destination])
 
     if result.returncode != 0:
         print(f"Error cloning {repo_name}")
 
-def clone_top_repositories():
+def clone_repositories(file_path, max_repos=5):
     create_clone_folder()
 
-    with open(INPUT_FILE, newline="", encoding="utf-8") as file:
+    with open(file_path, newline="", encoding="utf-8") as file:
         reader = csv.DictReader(file)
 
         for i, repo in enumerate(reader, start=1):
-
-            if i > MAX_REPOS:
+            if i > max_repos:
                 break
 
             repo_url = repo["url"]
             repo_name = f"{repo.get('owner')}_{repo.get('repository')}"
-            clone_repository(repo_url, repo_name, i)
+            clone_repository(repo_url, repo_name, i, max_repos)
 
 if __name__ == "__main__":
-    clone_top_repositories()
+    clone_repositories("data/raw/repositories.csv", max_repos=2)
--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
 from github_api.repositories import fetch_repositories
 from processing.metrics import extract_metrics
+from clone.clone_repositories import clone_repositories
 from utils.save_csv import save_to_csv
 from utils.load_query import load_query
 
 def main():
-
     query = load_query("src/github_api/queries/top_java_repositories.graphql")
 
     nodes = fetch_repositories(query, 1000)
 
     repositories = [extract_metrics(repo) for repo in nodes]
 
-    save_to_csv(repositories, "data/raw/repositories.csv")
+    csv_file = "data/raw/repositories.csv"
+    save_to_csv(repositories, csv_file)
+
+    clone_repositories(csv_file, max_repos=5)
 
 if __name__ == "__main__":
     main()
